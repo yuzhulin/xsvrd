091a7cd4daeda67c6ffff14e95ef716f9e355880
xushvai <xushvai@gmail.com>
2014-11-17 15:46:11 +0800

    add ParseCommand func to netio proj
    Signed-off-by: xushvai <xushvai@gmail.com>

--- framework/netio/main.cpp
+++ framework/netio/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include "../../include/os.h"
 
-
 void ParseCommand(int32 argc, char** argv);
 
 int32 main(int32 argc, char** argv)
 {
 	ParseCommand(argc, argv);
-
 
 	std::cin.get();
     return 0;
@@ -15,7 +13,12 @@ int32 main(int32 argc, char** argv)
 
 void ParseCommand(int32 argc, char** argv)
 {
-	/*if ((argc > 1) && (strcasecmp))
+	// you can use option "-v" to check the build version
+	////////////////////////////////////////////////////////////////
+	if ((argc > 1) && (strcasecmp(argv[1], "-v")))
 	{
-	}*/
+		std::clog << "build in [" << __DATE__ << ' ' << __TIME__
+			<< "](gcc" << __GNUC__ << '.' << __GNUC_MINOR__
+				<< ")" << 8 * sizeof(long) << "bit" << std::endl;
+	}
 }
--- include/os.h
+++ include/os.h
@@ -10,14 +10,15 @@ typedef std::uint8_t	uint8;
 typedef std::uint16_t	uint16;
 typedef std::uint32_t	uint32;
 
-#ifdef WIN32
+#ifdef _WIN32
+#define strcasecmp		strcmp
+
+#define __GNUC__		1
+#define __GNUC_MINOR__	0
 
 #endif
 
 #ifdef __APPLE__
-
 #endif
-
-
 
 #endif // _MINISVRD_INCLUDE_OS_H_
