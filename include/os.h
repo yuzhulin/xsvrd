b79fe2b66d203be26307f1b0aaa98d1f15915ead
xushuai <xushvai@gmail.com>
2014-12-03 15:43:07 +0800

    -e
    Signed-off-by: xushuai <xushvai@gmail.com>

--- include/os.h
+++ include/os.h
@@ -1,14 +1,14 @@
 #ifndef _XSVRD_INCLUDE_OS_H_
 #define _XSVRD_INCLUDE_OS_H_
 
-#include <cstdint>
+#include <stdint.h>
 
-typedef std::int8_t     int8;
-typedef std::int16_t	int16;
-typedef std::int32_t	int32;
-typedef std::uint8_t	uint8;
-typedef std::uint16_t	uint16;
-typedef std::uint32_t	uint32;
+typedef int8_t     int8;
+typedef int16_t	int16;
+typedef int32_t	int32;
+typedef uint8_t	uint8;
+typedef uint16_t	uint16;
+typedef uint32_t	uint32;
 
 
 
