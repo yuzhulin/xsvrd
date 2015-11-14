4b19703d04cf25835843aa93a2da4bcd06bcee81
xushuai_rmbp <xushvai@gmail.com>
2015-11-14 23:57:21 +0800

    some change.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

--- include/libcommon/x_socket.h
+++ include/libcommon/x_socket.h
@@ -11,24 +11,40 @@
 #include "../../include/os_refactor.h"
 #endif
 
+#define SEND_BUFFER_SIZE     (260 * 1024 - 2)
 // --------------------------------------------------------
 int SetSocketOption(SOCKET socket, int level, int optname, const void* optval, int optlen)
 {
 	int retval = -1;
 #ifdef _WIN32
 	retval = setsockopt(socket, level, optname,
-		static_cast<const char*>(optval), optlen);
+			static_cast<const char*>(optval), optlen);
+#elif __linux__
+	retval = setsockopt(socket, level, optname, optval, optlen);
 #endif
-
 	return retval;
 }
+
+int GetSocketOption(SOCKET socket, int level, int optname, void* optval, int*optlen)
+{
+	int retval = -1;
+#ifdef _WIN32
+	retval = getsockopt(socket, level, optname,
+ 			static_cast<char*>(optval), optlen);
+#elif __linux__
+	retval = getsockopt(socket, level, optname, optval, optlen); 
+#endif
+	return retval;
+}
+
 int CloseSocket(SOCKET socket)
 {
 	int retval = -1;
 #ifdef _WIN32
 	retval = closesocket(socket);
-#endif
+#else
 	retval = close(socket);
+#endif
 	return retval;
 }
 
@@ -65,6 +81,8 @@ private:
 	char server_ip_str_[IP_LENGTH];
 	int post_begin_;
 	int post_end_;
+	int read_begin_;
+	int read_end_;
 	unsigned int recv_pack_count_;
 	unsigned char* byte_post_buffer_;
 };
@@ -79,15 +97,17 @@ TCPSocket::~TCPSocket()
 
 void TCPSocket::Init(unsigned int recv_buf_len)
 {
-	socket_fd_        = INVALID_SOCKET;
-	socket_type_      = ST_COMMON;
-	socket_status_    = SS_CLOSED;
-	server_port_      = 0;
 	post_begin_       = 0;
 	post_end_         = 0;
+	read_begin_       = 0;
+	read_end_         = 0;
 	server_ip_        = 0;
+	server_port_      = 0;
 	recv_pack_count_  = 0;
 	byte_post_buffer_ = NULL;
+	socket_type_      = ST_COMMON;
+	socket_status_    = SS_CLOSED;
+	socket_fd_        = INVALID_SOCKET;
 	memset(&server_ip_str_, 0, sizeof(server_ip_str_));
 }
 
@@ -114,9 +134,23 @@ int TCPSocket::CreateClient(char* local_addr)
 		memset(&addr, 0, sizeof(addr));
 		addr.sin_family = AF_INET; 
 		addr.sin_addr.s_addr = inet_addr(local_addr);
-		bind(socket_fd_, (const struct sockaddr*)(&addr), sizeof(addr));
+		bind(socket_fd_, (const struct sockaddr*)&addr, sizeof(addr));
 	}
-	//if (setsockopt(socket_fd_, SOL_SOCKET, SO_SNDBUF, 
+	int option_value = SEND_BUFFER_SIZE;
+	int option_len = sizeof(option_value);
+	if (SetSocketOption(socket_fd_, SOL_SOCKET,
+		SO_SNDBUF, &option_value, option_len)) {
+		std::clog << "Set socket's send buffer size to " 
+	 		<< option_value << "failed!" << std::endl;
+		return -1;
+	}
+	if (!GetSocketOption(socket_fd_, SOL_SOCKET,
+		SO_SNDBUF, &option_value, &option_len)) {
+		std::clog << "Socket's send buffer is "
+			<< option_value << std::endl;
+	}
+	read_begin_ = read_end_ = 0;
+	post_begin_ = post_end_ = 0;
 	return 0;
 }
 
