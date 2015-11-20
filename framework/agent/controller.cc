5cc36bcd1b4b52dbe7475cb293999d048f8a5b6d
xushuai <xushvai@gmail.com>
2015-11-21 00:47:01 +0800

    some change.
    Signed-off-by: xushuai <xushvai@gmail.com>

--- framework/agent/controller.cc
+++ framework/agent/controller.cc
@@ -5,11 +5,35 @@ namespace xsvrd {
 
 Controller::Controller()
 {
-	std::clog << "sf" << std::endl;
+	dbsvrd_handler_thread_ = NULL;
+	mainsvrd_handler_thread_ = NULL;
+	othersvrd_handler_thread_ = NULL;
 }
 
 Controller::~Controller()
 {
+	if (dbsvrd_handler_thread_) {
+		delete dbsvrd_handler_thread_;
+		dbsvrd_handler_thread_ = NULL;
+	}
+	if (mainsvrd_handler_thread_) {
+		delete mainsvrd_handler_thread_;
+		mainsvrd_handler_thread_ = NULL;
+	}
+	if (othersvrd_handler_thread_) {
+		delete othersvrd_handler_thread_;
+		othersvrd_handler_thread_ = NULL;
+	}
+}
+
+int Controller::PrepareToRun()
+{
+	return 0;
+}
+
+int Controller::Run()
+{
+	return 0;
 }
 
 }
--- framework/agent/controller.h
+++ framework/agent/controller.h
@@ -1,5 +1,6 @@
 #ifndef __CONTROLLER_H__
 #define __CONTROLLER_H__
+#include "handlerthread.h"
 
 namespace xsvrd {
 
@@ -7,6 +8,14 @@ class Controller {
 public:
 	Controller();
 	virtual~Controller();
+
+	int Run();
+	int PrepareToRun();
+
+private:
+	HandlerThread* mainsvrd_handler_thread_;
+	HandlerThread* dbsvrd_handler_thread_;
+	HandlerThread* othersvrd_handler_thread_;
 };
 
 }
