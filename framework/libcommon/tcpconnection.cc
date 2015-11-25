f21609119e941a0517a4575c8a2d6035592cdeb2
xushuai <xushvai@gmail.com>
2015-11-25 04:55:24 -0500

    implement TCPConnection::Init.
    Signed-off-by: xushuai <xushvai@gmail.com>

--- framework/libcommon/tcpconnection.cc
+++ framework/libcommon/tcpconnection.cc
@@ -4,10 +4,24 @@ namespace xsvrd {
 
 TCPConnection::TCPConnection()
 {
+	port_ = 0;
+	addr_.clear();
+	entity_id_ = 0;
+	entity_type_ = CET_UNKNOWN;
 }
 
 TCPConnection::~TCPConnection()
 {
+}
+
+int TCPConnection::Init(ConnectionEntityType entity_type, 
+	int entity_id, std::string addr, unsigned short port)
+{
+	addr_ = addr;
+	port_ = port;
+	entity_id_ = entity_id;
+	entity_type_ = entity_type;
+	return 0;
 }
 
 }
--- include/libcommon/tcpconnection.h
+++ include/libcommon/tcpconnection.h
@@ -1,5 +1,6 @@
 #ifndef __TCPCONNECTION_H__
 #define __TCPCONNECTION_H__
+#include <string>
 
 namespace xsvrd {
 
@@ -7,6 +8,14 @@ class TCPConnection {
 public:
 	TCPConnection();
 	virtual ~TCPConnection();
+	int Init(ConnectionEntityType entity_type, 
+		int entity_id, std::string addr, unsigned short port);
+
+private:
+	unsigned short port_;
+	std::string addr_;
+	int entity_id_;
+	ConnectionEntityType entity_type_;
 };
 
 }
